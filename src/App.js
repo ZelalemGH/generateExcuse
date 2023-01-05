bab856ca7f7c47e02549dcb839be2a77bdc1393a
zelalem Habtegiorgis <zeegiorgis@gmail.com>
2023-01-05 13:11:52 -0500

    generating different excuse from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Axios from 'axios'
+import { useState } from 'react'
 
 function App() {
+  const [generatedExcuse, setGeneratedExcuse] = useState('')
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+      (res) => {
+        setGeneratedExcuse(res.data[0].excuse)
+      },
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1> Generate an Excuse </h1>
+      <button onClick={() => fetchExcuse('family')}> Family </button>
+      <br />
+      <button onClick={() => fetchExcuse('office')}> Office </button>
 
-export default App;
+      <p>{generatedExcuse}</p>
+    </div>
+  )
+}
+export default App
